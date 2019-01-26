3a94f01e7d5688cc011b8198c75081e3c9cbe8da
Gary Williams <gary@lbm.co>
2019-01-25 19:16:12 -0500

    lazy loading

--- src/directives/image.js
+++ src/directives/image.js
@@ -3,55 +3,78 @@ export default function(el, binding, vnode) {
   let srcAttribute = binding.value.srcAttr;
   let breakpoint = binding.value.breakpoint;
   let background = binding.modifiers.background;
+  let lazy = binding.modifiers.lazy;
   let noSize = binding.modifiers.nosize;
 
   breakpoint = breakpoint !== null ? breakpoint : 'desktop';
   srcAttribute = srcAttribute ? srcAttribute : 'src';
 
-  let theImageSize = () => {
-    let sizes = image.sizes;
+  let setImage = () => {
+    // Get the image size
+    let theSize = theImageSize();
 
-    for (const size in sizes) {
-      if (sizes.hasOwnProperty(size)) {
-        const sizeSettings = sizes[size];
+    // Default the image the url but if it does have a size match
+    // set that instead
+    let theImage = image.url;
+    if (theSize) {
+      // In the case where sizes are set but the url is manual
+      // (or maybe the image media wasn't generated) this will ensure
+      // the url is at least present from the media property
+      if (image.media[theSize.size]) {
+        theImage = image.media[theSize.size];
+      }
+    }
 
-        // If breakpoints isn't set assume only one size and return it
-        if (!sizeSettings.breakpoints) {
-          return { size: size, settings: sizeSettings };
-        }
-        if (sizeSettings.breakpoints.includes(breakpoint)) {
-          return { size: size, settings: sizeSettings };
-        }
+    if (background) {
+      el.style.backgroundImage = `url(${theImage})`;
+      console.log('loaded');
+    } else {
+      el[srcAttribute] = theImage;
+
+      if (image.alt) {
+        el.alt = image.alt;
+      }
+
+      if (!noSize && theSize) {
+        el.width = theSize.settings.w;
+        el.height = theSize.settings.h;
       }
     }
   };
 
-  let theSize = theImageSize();
+  let theImageSize = () => {
+    let sizes = image.sizes;
 
-  // Default the image the url but if it does have a size match
-  // set that instead
-  let theImage = image.url;
-  if (theSize) {
-    // In the case where sizes are set but the url is manual
-    // (or maybe the image media wasn't generated) this will ensure
-    // the url is at least present from the media property
-    if (image.media[theSize.size]) {
-      theImage = image.media[theSize.size];
+    // Search the sizes for the right size based on the current breakpoint
+    for (const size in sizes) {
+      if (sizes.hasOwnProperty(size)) {
+        const sizeSettings = sizes[size];
+
+        if (sizeSettings.breakpoints && sizeSettings.breakpoints.includes(breakpoint)) {
+          return { size: size, settings: sizeSettings };
+        }
+        // If breakpoints isn't set assume only one size and return it
+      }
     }
-  }
 
-  if (background) {
-    el.style.backgroundImage = `url(${theImage})`;
+    // We couldn't find the size so return the first one
+    let defaultSize = Object.keys(sizes)[0];
+    return { size: defaultSize, settings: sizes[defaultSize] };
+  };
+
+  if (lazy) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          setImage();
+
+          lazyImageObserver.unobserve(el);
+        }
+      });
+    });
+
+    lazyImageObserver.observe(el);
   } else {
-    el[srcAttribute] = theImage;
-
-    if (image.alt) {
-      el.alt = image.alt;
-    }
-
-    if (!noSize && theSize) {
-      el.width = theSize.settings.w;
-      el.height = theSize.settings.h;
-    }
+    setImage();
   }
 }
